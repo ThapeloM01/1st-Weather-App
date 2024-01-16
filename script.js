106ea3a7ca188ef9207ea49cd6e04b6b0235ff9c
ThapeloM01 <sinahthapelo01@gmail.com>
2024-01-16 22:09:37 +0200

    Used an API on my search form to update on weather of any city

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
+function weatherResponse(response) {
+    let temperatureNumber = document.querySelector("#temperature-value");
+    let temperature = response.data.temperature.current;
+    temperatureNumber.innerHTML = Math.round(temperature);
+    let cityResult = document.querySelector("#weather-city");
+    cityResult.innerHTML = response.data.city;
+}
+
+function formResult(city) {
+    let apiKey = "aod048etf33b73a958b75839ca060b4e";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+   axios.get(apiUrl).then(cityresponse)
+}
 
 function WeatherSearch(event) {
     event.preventDefault();
     let searchResult = document.querySelector("#serch-input")
      let cityResult = document.querySelector("#weather-city")
-    cityResult.innerHTML = searchResult.ariaValueMax;
-
+    cityResult.innerHTML = searchResult.value;
+formResult(searchResult.value);
     }let formSearchElement = document.querySelector("form-search");
-formSearchElement.addEventListener("submit",WeatherSe
+formSearchElement.addEventListener("submit",WeatherSearch);
+
+formResult(Canada)
