8c199273435649696ab68fb6f4784d819827e6bf
ThapeloM01 <sinahthapelo01@gmail.com>
2024-01-16 23:25:16 +0200

    added js functions

--- script.js
+++ script.js
@@ -4,12 +4,39 @@ function weatherResponse(response) {
     temperatureNumber.innerHTML = Math.round(temperature);
     let cityResult = document.querySelector("#weather-city");
     cityResult.innerHTML = response.data.city;
+    let description = document.querySelector("weather-Description");
+    description.innerHTML = response.data.condition.description;
+    let humidity = document.querySelector("humidityInfo");
+    humidity.innerHTML = `${response.data.temperature.humidity}%`;
+    let windSpeed = document.querySelector("#speed-wind");
+    windSpeed.innerHTML = `${response.data.wind.speed}km/h`;
+    let time = document.querySelector("#time-details");
+    let date = new Date(response.data.time * 1000);
+
+    time.innerHTML = dateFormat[date];
+    let weatherIcon = document.querySelector("#icon")
+    weatherIcon.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+    
+}
+function dateFormat(date) {
+    let hours = date.getHours;
+    let minutes = date.getMinutes;
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"
+];
+let day = days[date.getDay()];
+
+if (minutes < 10){
+    `minutes = 0${minutes}`;
+}
+ 
+return `${day} ${hours}:${minutes};
+
 }
 
 function formResult(city) {
     let apiKey = "aod048etf33b73a958b75839ca060b4e";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-   axios.get(apiUrl).then(cityresponse)
+    let apiUrl = https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+   axios.get(apiUrl).then(cityresponse);
 }
 
 function WeatherSearch(event) {
